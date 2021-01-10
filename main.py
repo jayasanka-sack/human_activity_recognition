83b1237984bd663b2f2c36868000b9e8d3776989
Jayasanka Weerasinghe <jayasanka.sack@gmail.com>
2021-01-10 12:09:53 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,86 @@
+from tensorflow import keras
+from sklearn import preprocessing
+import numpy as np
+import pandas as pd
+from scipy import stats
+
+normalization_max = 66.615074
+normalization_min = -78.47761
+labels = ['A',
+          'B',
+          'C',
+          'D',
+          'E']
+activities = {
+    "A": "walking",
+    "B": "jogging",
+    "C": "stairs",
+    "D": "sitting",
+    "E": "standing"
+}
+TIME_PERIODS = 200
+STEP_DISTANCE = 100
+test_segments = []
+test_labels = []
+
+
+def create_segments_and_labels(dff, time_steps, step):
+    print('Creating Segments...')
+    # x, y, z acceleration as features
+    n_features = 3
+    # Number of steps to advance in each iteration (for me, it should always
+    # be equal to the time_steps in order to have no overlap between segments)
+    # step = time_steps
+    segments = []
+    labels = []
+    accel_data = dff[['x-axis', 'y-axis', 'z-axis']]
+    for i in range(0, len(dff) - time_steps, step):
+        values = accel_data.iloc[i:(i + time_steps)].values
+        # Retrieve the most often used label in this segment
+        label = stats.mode(dff['activity'][i: i + time_steps])[0][0]
+        segments.append(values)
+        labels.append(label)
+
+    # Bring the segments into a better shape
+    reshaped_segments = np.asarray(segments, dtype=np.float32).reshape(-1, time_steps, n_features)
+    labels = np.asarray(labels)
+    return reshaped_segments, labels
+
+
+def normalise_data(df):
+    print('Normalising Data...')
+    ndf = df.copy()
+    ndf['x-axis'] = (df['x-axis'] - normalization_min) / (normalization_max - normalization_min)
+    ndf['y-axis'] = (df['y-axis'] - normalization_min) / (normalization_max - normalization_min)
+    ndf['z-axis'] = (df['z-axis'] - normalization_min) / (normalization_max - normalization_min)
+    ndf = ndf.round({'x-axis': 4, 'y-axis': 4, 'z-axis': 4})
+    return ndf
+
+
+def read_data():
+    global test_segments, test_labels
+    column_names = ['activity',
+                    'timestamp',
+                    'x-axis',
+                    'y-axis',
+                    'z-axis']
+    print('Reading Data...')
+    df = pd.read_csv('data_compact.csv', header=None, names=column_names)
+    ndf = normalise_data(df)
+    test_segments, test_labels = create_segments_and_labels(ndf, TIME_PERIODS, STEP_DISTANCE)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    read_data()
+    model = keras.models.load_model("model")
+    le = preprocessing.LabelEncoder()
+    le.fit(labels)
+    while True:
+        index = int(input("Enter an index:"))
+        encoded_prediction = np.argmax(model.predict(np.array([test_segments[0]])), axis=1)
+        decoded_prediction = le.inverse_transform(encoded_prediction)[0]
+        print("Activity: " + activities[test_labels[index]])
+        print("Predicted Activity: " + activities[decoded_prediction])
+
+
